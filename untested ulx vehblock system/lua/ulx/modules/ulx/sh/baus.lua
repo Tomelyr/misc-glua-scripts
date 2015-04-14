a1a3f0a8858edd6209ec95183d77471aec7f0adf
Tomelyr <Tomelyr@googlemail.com>
2015-04-14 23:06:31 +0200

    saving the ban

--- untested ulx vehblock system/lua/ulx/modules/ulx/sh/baus.lua
+++ untested ulx vehblock system/lua/ulx/modules/ulx/sh/baus.lua
@@ -4,18 +4,34 @@ local function BlockVehicle( ply, veh, _ )
 			ply.IsVehBlocked = false
 			return true
 		else
-			ply:ChatPrint("Du bist noch" .. CurTime() - ply.VehBlockedTime .." Sekunden gebannt.")
+			ply:ChatPrint("Du bist noch" .. math.ceil(CurTime() - ply.VehBlockedTime) .." Sekunden gebannt."
 			return false
 		end
 	end
 end
 hook.Add("CanPlayerEnterVehicle", "VehBlockScript", BlockVehicle)
 
+gameevent.Listen( "player_disconnect" )
+hook.Add( "player_disconnect", "BausVehBlockDC", function( data )
+	local steamid = data.networkid
+	if data.IsVehBlocked then
+		file.Write("vehblocked/" .. steamid, data.VehBlockedTime)
+	end
+end
+gameevent.Listen( "player_connect" )
+hook.Add( "player_connect", "BausVehBlockConnect", function( data )
+	local steamid = data.networkid
+	if file.Exists("vehblocked/" .. steamid, "DATA") then
+		local time = file.Read("vehblocked/" .. steamid, "DATA")
+		data.IsVehBlocked = time
+	end
+end
 
 function ulx.vehblock(ply, targets)
 	for _,v in pairs(targets) do
 		v.IsVehBlocked = true
 		v.VehBlockedTime = CurTime() + 7200
+		ply:ChatPrint("Vehicle Banned:" ..v:Name())
 	end
 
 end
@@ -23,3 +39,16 @@ local vehblock = ulx.command("Baus Kingski", "ulx vehblock", ulx.vehblock, "!veh
 vehblock:addParam({ type=ULib.cmds.PlayersArg })
 vehblock:defaultAccess(ULib.ACCESS_SUPERADMIN)
 vehblock:help("VehBlock.")
+
+function ulx.vehunblock(ply, targets)
+	for _,v in pairs(targets) do
+		v.IsVehBlocked = false
+		v.VehBlockedTime = CurTime()
+		ply:ChatPrint("Vehicle Unbanned:" ..v:Name())
+	end
+
+end
+local vehunblock = ulx.command("Baus Kingski", "ulx vehunblock", ulx.vehunblock, "!vehunblock")
+vehunblock:addParam({ type=ULib.cmds.PlayersArg })
+vehunblock:defaultAccess(ULib.ACCESS_SUPERADMIN)
+vehunblock:help("VehUnBlock.")
